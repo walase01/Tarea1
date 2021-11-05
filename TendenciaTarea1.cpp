637dcdf2c79db428dbe283bfdc983416933b7c8d
= <=>
2021-11-05 19:34:22 -0400

    adding code

--- TendenciaTarea1.cpp
+++ TendenciaTarea1.cpp
@@ -2,8 +2,51 @@
 
 using namespace std;
 
+bool primo(int numero);
+
 int main(){
 	
+	int n = 1;
+	
+	int fibo = 1;
+	int otro = 1;
+	int suma = 0;
+	string paroimpar;
+	
+	cout<<0<<". "<<fibo<<": "<<"impar"<<" no es primo"<<"\n";
+	while(n < 50){	
+
+		suma = fibo + otro;
+		if(suma % 2 == 0){
+			paroimpar = "par";
+		}
+		else{
+			paroimpar = "impar";
+		}
+		if(primo(suma) == true){
+			paroimpar += " primo";
+		}
+		else{
+			paroimpar += " no es primo";
+		}
+		cout<<n<<". "<<suma<<": "<<paroimpar<<"\n";
+		otro=fibo;
+		fibo = suma;
+		n++;
+	}
 	
 	return 0;
+}
+
+
+bool primo(int numero){
+	
+	if (numero == 0 || numero == 1 || numero == 4) return false;
+  	
+	for (int x = 2; x < numero / 2; x++) {
+		
+    if (numero % x == 0) return false;
+    
+    }
+    return true;	
 }
